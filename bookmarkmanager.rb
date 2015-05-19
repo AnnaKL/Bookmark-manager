d6002f0f755963bc429c05c9747a3e47a5f2b322
Anna <anna.klimas@gmail.com>
2015-05-19 23:54:09 +0100

    stage 9 completed.

--- bookmarkmanager.rb
+++ bookmarkmanager.rb
@@ -19,6 +19,7 @@ class BookmarkManager < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
   use Rack::Flash, :sweep => true
+  use Rack::MethodOverride
 
 
   get '/' do
@@ -56,6 +57,28 @@ class BookmarkManager < Sinatra::Base
     flash.now[:errors] = @user.errors.full_messages
     erb :'user/new'
   end
+  end
+
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions' do
+  email, password = params[:email], params[:password]
+  user = User.authenticate(email, password)
+  if user
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    flash[:errors] = ['The email or password is incorrect']
+    erb :'sessions/new'
+  end
+end
+
+delete '/sessions' do
+    flash[:notice] = 'Good bye!'
+    session['user_id'] = nil
+    redirect to ('/')
   end
 
   helpers do
--- lib/user.rb
+++ lib/user.rb
@@ -13,8 +13,17 @@ class User
   validates_confirmation_of :password
   validates_uniqueness_of :email
 
+def self.authenticate(email, password)
+  user = first(email: email)
+  if user && BCrypt::Password.new(user.password_digest) == password
+    user
+  else
+    nil
+  end
+end
+
 def password=(password)
   @password = password
   self.password_digest = BCrypt::Password.create(password)
-end
+  end
 end
